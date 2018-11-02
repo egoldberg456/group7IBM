82133b5321d8b0961a27ac2cf35726d5e7d0b451
Ethan Goldberg <gethan@umich.edu>
2018-11-02 08:58:39 -0500

    Here

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
 from group7IBM import *
+from flask import request
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/query/', methods=['POST', 'GET'])
+@app.route('/query/', methods=['POST'])
 def hit_it():
+    query = request.form['query']
     
+
+
