f5925f9e1929f178e2c0f1b7d54bf794f05c7785
Ethan Goldberg <gethan@umich.edu>
2018-11-02 09:36:45 -0500

    LESSGO

--- index.py
+++ index.py
@@ -1,13 +1,30 @@
 from group7IBM import *
 from flask import request
+import json
+
+def parse_json():
+    with open('questionnaire.json', 'r') as f:
+        dict = json.load(f)
+
+    print(dict)
+    return dict
+
+def give_answer(query):
+    dict = parse_json()
+    for word in query.split(' '):
+
+        if word in dict['contextWords']:
+            question = dict['contextWords'][word]
+            answer = dict['questionnaire'][question]['answer']
+            return answer
+
+    return "Sorry, we do not have an answer to this question"
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/query/', methods=['POST'])
+@app.route('/query/', methods=['POST', 'GET'])
 def hit_it():
     query = request.form['query']
-    
-
-
+    return give_answer(query)
